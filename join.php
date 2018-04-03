de4055e32aaaa114b5f8e93780ca7a9d6314f13c
Unknown <gross.tadeas@gmail.com>
2018-04-03 18:56:52 +0200

    1.1.5
    Přidáno hashování hesla

--- join.php
+++ join.php
@@ -3,7 +3,7 @@ header("Content-Type: text/html; charset=windows-1250");
 echo "<a href='index.php'><img src='./poodle_logo2.bmp' height ='180' width '360'   /></a><br>\n";
 ?>
 <head>
-
+<script src='https://www.google.com/recaptcha/api.js'> </script>
 <div class="menu">
   <a href="index.php">STAHOVÁNÍ</a>
   <a href="upload.php">NAHRÁVÁNÍ</a>
@@ -13,23 +13,26 @@ echo "<a href='index.php'><img src='./poodle_logo2.bmp' height ='180' width '360
  <p>
 REGISTRACE
  <p>
-<form id="register" action="join.php" method="post">
-<input type="hidden" name="submitted" id="submitted" value="1">
+<form action="" method="post"/>
 Vaše jméno a pøíjmení:
-<input type="text" name="name" id="name" maxlenght ="50">
+<input type="text" name="name"/>
  <p>
 Vaše emailová adresa:
-<input type="text" name="mail" id="mail" maxlenght ="50">
+<input type="text" name="mail"/>
+ <p>
+-Zadejte svùj opravdový e-mail, jakékoliv otázky týkající se vašeho úètu, budou odesílány na vámi zadaný e-mail!
  <p>
 Vaše pøihlašovací jméno
-<input type="nic" name="nick" id="nick" maxlenght ="15">
+<input type="text" name="nick"/>
  <p>
 Vaše heslo:
-<input type="password" name="pass" id="pass" maxlenght ="20">
+<input type="password" name="pass"/>
  <p>
- <input type="submit" name="submit" value="Potvrdit">
+Prokažte, že jste èlovìk:
+<div class="g-recaptcha" data-sitekey="6LfH1EkUAAAAAAzJSozvSTJ9lKLW7359kJh36fXo"></div>
+<input type="submit" name="submit" value="Potvrdit"/>
 </form>
-
+</head>
 <style>
 /* Add a black background color to the top navigation */
 .a  {
@@ -67,5 +70,90 @@ Vaše heslo:
 
 </style>
 
+<?php
 
+$servername = "localhost";
+$conn = new mysqli($servername);
+
+if ($conn->connect_error) {
+  die("Pøipojení k MYSQL databázi selhalo, chyba: " . $conn->connect_error);
+}
+
+
+
+if (isset($_POST['submit'])) {
+  /* GOOGLE RECAPTCHA */
+  $secretKey = '6LfH1EkUAAAAACigDzk4Y_3AQdv6DSS7CW8DNIYb';
+  $name = $_POST['name'];
+  $mail = $_POST['mail'];
+  $nick = $_POST['nick'];
+  $pass = $_POST['pass']; 
+  $responseKey = $_POST['g-recaptcha-response'];
+  $ipKey = $_SERVER['REMOTE_ADDR'];
+  $send_url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$ipKey";
+  
+
+  $response = file_get_contents($send_url);
+  $response = json_decode($response);
+  
+  
+  
+  if($response->success) {
+   
+
+      $makeAccountOK = 0;
+      /** CHECK JMENA = POUZE PISMENA, MAX. DELKA 39 **/
+      if(!(preg_match("/[a-zA-Z'-]/",$name)) or (strlen($name)) > 40){
+          $makeAccountOK = 1;
+          echo "Špatnì zadané jméno a pøíjmení, zadané jméno nesmí mít více jak 40 znakù a zadávat mùžete pouze písmena.  <p>";
+          }
+      /** CHECK MAILU POMOCI FUNKCE, MAX. DELKA 49**/
+      if(!(filter_var($mail, FILTER_VALIDATE_EMAIL)) or (strlen($mail) > 50)){
+          $makeAccountOK = 2;
+          echo "Špatnì zadaný email, zadaný email nesmí mít více jak 50 znakù a zadávejte ho jen ve formì nazev@mail.koncovka  <p>";
+          }
+
+      /** CHECK NICKU, ctype_alnum KONTROLUJE POUZE ALFANUMERICKE, strlen trim == strlen NAM ZJISTI JESTLI JE STEJNA DELKA I PO SMAZANI MEZER, TIM PADEM ZADNE MEZERY, MAX. DELKA 24 **/    
+      if(!(ctype_alnum($nick)) or (strlen(preg_replace('/\s+/', '', $nick)) !== strlen($nick)) or strlen($nick) > 25){
+          $makeAccountOK = 3;
+          echo "Špatnì zadané uživatelské jméno, zadané už. jméno nesmí mít více jak 25 znakù a nesmíte zadávat mezery nebo speciální znaky (mimo èíslice).  <p>";
+          }
+
+      /** CHECK HESLA, strlen trim ZNOVA KONTROLUJE JESTLI OBSAHUJE MEZERY, MIN DELKA 8 a MAXIMALNI 24 **/
+
+      if(strlen($pass) < 7 or strlen($pass) > 25 or (strlen(preg_replace('/\s+/', '', $pass)) !== strlen($pass))){
+          echo "Špatnì zadané heslo, zadané heslo musí mít více jak 7 znakù, nesmí obsahovat mezery a nesmí být delší jak 25 znakù. <p>";
+          $makeAccountOK = 4;
+          }
+          
+      echo($makeAccountOK);
+      
+      /** pro vytvoøení databáze: CREATE TABLE `poodle`.`userLoginInformation` ( `id` INT NOT NULL , `username` VARCHAR NOT NULL , `passwordHash` VARCHAR NOT NULL , `firstLastName` VARCHAR NOT NULL , `email` VARCHAR NOT NULL , `accountBalance` VARCHAR NOT NULL) ENGINE = InnoDB; **/
+      
+      if($makeAccountOK == 0){
+      /** jsem solidní, tak to radši zahešuju**/
+        /** osolim to **/
+        $passSalted = $pass . $mail;
+        $hashOptions = [
+          'cost' => 11,
+        ];
+            echo password_hash($passSalted, PASSWORD_BCRYPT, $hashOptions);
+      
+      
+      }    
+  }
+  
+  
+
+   
+  else {
+    echo("Potvrïte že nejste robot pomocí reCAPTCHA.") ;
+    die;
+  }
+  
+   
+ }
+
+ 
+?>
 
